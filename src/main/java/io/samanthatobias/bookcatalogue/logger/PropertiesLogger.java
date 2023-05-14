c6f8227e6c0424bd8b2447cbf1c6086a3ad58bd7
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-05-14 22:16:21 +0200

    PropertiesLogger.java

---
+++ src/main/java/io/samanthatobias/bookcatalogue/logger/PropertiesLogger.java
@@ -0,0 +1,21 @@
+package io.samanthatobias.bookcatalogue.logger;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropertiesLogger {
+
+//	@Autowired
+//	public PropertiesLogger(Environment environment) {
+//		System.out.println("==== System properties ====");
+//		for (PropertySource<?> propertySource : ((AbstractEnvironment) environment).getPropertySources()) {
+//			if (propertySource instanceof MapPropertySource) {
+//				for (String key : ((MapPropertySource) propertySource).getPropertyNames()) {
+//					System.out.println(key + "=" + propertySource.getProperty(key));
+//				}
+//			}
+//		}
+//		System.out.println("===========================");
+//	}
+
+}
