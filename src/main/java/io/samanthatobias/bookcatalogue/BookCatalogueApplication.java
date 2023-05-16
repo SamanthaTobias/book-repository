6531e4a3842863ecfdc554c4e7367d8ea009099b
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-05-16 21:44:21 +0200

    fix

--- src/main/java/io/samanthatobias/bookcatalogue/BookCatalogueApplication.java
+++ src/main/java/io/samanthatobias/bookcatalogue/BookCatalogueApplication.java
@@ -1,9 +1,7 @@
 package io.samanthatobias.bookcatalogue;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -15,13 +13,6 @@ public class BookCatalogueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookCatalogueApplication.class, args);
-	}
-
-	@Bean
-	public BasicAuthInterceptor basicAuthInterceptor(
-			@Value("${basic.auth.username}") String username,
-			@Value("${basic.auth.password}") String password) {
-		return new BasicAuthInterceptor(username, password);
 	}
 
 	@Profile("local")
